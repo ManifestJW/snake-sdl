22398775693a886401bcc307d12b9f6e93f0b02c
ManifestJW <legorobin2005@gmail.com>
2026-01-22 21:07:23 -0500

    Add windows shit

---
+++ src/winmain.c
@@ -0,0 +1,20 @@
+#ifdef _WIN32
+#  include <stdlib.h>
+#  include <windows.h>
+
+extern int main(int argc, char **argv);
+extern int __argc;
+extern char **__argv;
+
+int WINAPI WinMain(HINSTANCE instance,
+                   HINSTANCE prev_instance,
+                   LPSTR cmd_line,
+                   int show_cmd) {
+  (void)instance;
+  (void)prev_instance;
+  (void)cmd_line;
+  (void)show_cmd;
+
+  return main(__argc, __argv);
+}
+#endif
